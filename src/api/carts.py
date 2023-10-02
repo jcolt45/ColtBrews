23507e4768ea4bde2ca3181e9caddc6c2664deb7
Jack Colt <jcolt@calpoly.edu>
2023-10-02 13:54:50 -0700

    Version1

--- src/api/carts.py
+++ src/api/carts.py
@@ -16,102 +16,29 @@ class Cart(BaseModel):
     items : List[str]
     quantity : List[int]
 
-class Node:
-    def __init__(self, key:str, value:Cart):
-        self.key = key
-        self.value = value
-        self.next = None
-  
-class HashTable:
-    """
-    Hash Table based off implemention on geeksforgeeks.org
-    """
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.size = 0
-        self.table = [None] * capacity
-  
-    def _hash(self, key):
-        return hash(key) % self.capacity
-  
-    def insert(self, key, value):
-        index = self._hash(key)
-  
-        if self.table[index] is None:
-            self.table[index] = Node(key, value)
-            self.size += 1
-        else:
-            current = self.table[index]
-            while current:
-                if current.key == key:
-                    current.value = value
-                    return
-                current = current.next
-            new_node = Node(key, value)
-            new_node.next = self.table[index]
-            self.table[index] = new_node
-            self.size += 1
-  
-    def search(self, key):
-        index = self._hash(key)
-  
-        current = self.table[index]
-        while current:
-            if current.key == key:
-                return current.value
-            current = current.next
-  
-        raise KeyError(key)
-  
-    def remove(self, key):
-        index = self._hash(key)
-  
-        previous = None
-        current = self.table[index]
-  
-        while current:
-            if current.key == key:
-                if previous:
-                    previous.next = current.next
-                else:
-                    self.table[index] = current.next
-                self.size -= 1
-                return
-            previous = current
-            current = current.next
-  
-        raise KeyError(key)
-  
-    def __len__(self):
-        return self.size
-  
-    def __contains__(self, key):
-        try:
-            self.search(key)
-            return True
-        except KeyError:
-            return False
-
-ht = HashTable(100)
-
 class NewCart(BaseModel):
     customer: str
+
+c_id = 0
+carts:List[Cart] = []
 
 @router.post("/")
 def create_cart(new_cart: NewCart):
     """ """
-    newC = Cart
-    newC.items = []
-    newC.quantity = []
-    ht.insert(NewCart.customer, newC)
-    return {"cart_id": NewCart.customer}
+    new_id = c_id
+    c_id += 1
+    new_c = Cart
+    new_c.items = []
+    new_c.quantity = []
+    carts.append(new_c)
+    return {"cart_id": new_id}
 
 
 @router.get("/{cart_id}")
-def get_cart(cart_id: str):
+def get_cart(cart_id: int):
     """ """
-    cart:Cart = ht.search(cart_id)
-    return {"items": cart.items, "quantity": cart.quantity}
+    cur_cart = carts[cart_id]
+    return {"items": cur_cart.items, "quantity": cur_cart.quantity}
 
 
 class CartItem(BaseModel):
@@ -119,11 +46,11 @@ class CartItem(BaseModel):
 
 
 @router.post("/{cart_id}/items/{item_sku}")
-def set_item_quantity(cart_id: str, item_sku: str, cart_item: CartItem):
+def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
-    cart:Cart = ht.search(cart_id)
-    cart.items.append(item_sku)
-    cart.quantity.append(cart_item.quantity)
+    cur_cart = carts[cart_id]
+    cur_cart.items.append(item_sku)
+    cur_cart.quantity.append(cart_item.quantity)
     return "OK"
 
 
@@ -133,13 +60,13 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
-    cart:Cart = ht.search(cart_id)
+    cur_cart = carts[cart_id]
     index = 0
     cost = 0
     total_potions = 0
-    for item in cart.items:
+    for item in cur_cart.items:
         if item == "RED_POTION_0":
-            cart_red_potions = cart.quantity[index]
+            cart_red_potions = cur_cart.quantity[index]
             total_potions += cart_red_potions
             cost += (50 * cart_red_potions)
         index += 1
@@ -154,5 +81,4 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = %d" % (bank)))
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = %d" % (cur_red_potions)))
 
-    ht.remove(cart_id)
     return {"total_potions_bought": total_potions, "total_gold_paid": cost}
