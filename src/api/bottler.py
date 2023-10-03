3f72dd2eab2913d1f97949f2548fe393a1fc26df
Jack Colt <jcolt@calpoly.edu>
2023-10-02 18:21:06 -0700

    Version1

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -20,14 +20,17 @@ class PotionInventory(BaseModel):
 def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     """ """
     print(potions_delivered)
-    for potion in potions_delivered:
-        if potion.potion_type == [100, 0, 0, 0]:
-            new_red_bots = potion.quantity
 
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
         first_row = result.first()
-        cur_red_potions = first_row.num_red_potions + new_red_bots
+        cur_red_ml = first_row.num_red_ml
+        cur_red_potions = first_row.num_red_potions
+        for potion in potions_delivered:
+            if potion.potion_type == [100, 0, 0, 0]:
+                cur_red_potions += potion.quantity
+                cur_red_ml -= (100 * potion.quantity)
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = %d" % (cur_red_ml)))
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = %d" % (cur_red_potions)))
 
     return "OK"
@@ -45,16 +48,14 @@ def get_bottle_plan():
 
     # Initial logic: bottle all barrels into red potions.
 
-    cur_red_ml = 0
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
         first_row = result.first()
-        cur_red_ml += first_row.num_red_ml
+        cur_red_ml = first_row.num_red_ml
         new_red_bots = 0
         while (cur_red_ml >= 100):
             new_red_bots += 1
             cur_red_ml -= 100
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = %d" % (cur_red_ml)))
 
     return [
             {
