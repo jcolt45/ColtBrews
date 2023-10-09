5adbe07c204f1bdd9752909c747c368bb0589e9a
Jack Colt <jcolt@calpoly.edu>
2023-10-09 13:17:39 -0700

    Version2

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -34,11 +34,11 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
         add_blue_ml = first_row.num_blue_ml
 
         for barrel in barrels_delivered:
-            if barrel.sku == "SMALL_RED_BARREL":
+            if barrel.potion_type == [100, 0, 0, 0]:
                 add_red_ml += (barrel.ml_per_barrel * barrel.quantity)
-            if barrel.sku == "SMALL_GREEN_BARREL":
+            if barrel.potion_type == [0, 100, 0, 0]:
                 add_green_ml += (barrel.ml_per_barrel * barrel.quantity)
-            if barrel.sku == "SMALL_BLUE_BARREL":
+            if barrel.potion_type == [0, 0, 100, 0]:
                 add_blue_ml += (barrel.ml_per_barrel * barrel.quantity)
             bank -= (barrel.price * barrel.quantity)
 
@@ -63,36 +63,71 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         cur_green_potions = first_row.num_green_potions
         cur_blue_potions = first_row.num_blue_potions
 
+        mini_red = "MINI_RED_BARREL"
+        mini_red_num = 0
         small_red = "SMALL_RED_BARREL"
         small_red_num = 0
+
+        mini_green = "MINI_GREEN_BARREL"
+        mini_green_num = 0
         small_green = "SMALL_GREEN_BARREL"
         small_green_num = 0
+
+        mini_blue = "MINI_BLUE_BARREL"
+        mini_blue_num = 0
         small_blue = "SMALL_BLUE_BARREL"
         small_blue_num = 0
+
         for barrel in wholesale_catalog:
-            if (barrel.sku == small_red) & (cur_red_potions < 10):
-                if (bank >= barrel.price) & (barrel.quantity > 0):
+            if (barrel.potion_type == [100, 0 , 0, 0]) & (cur_red_potions < 10):
+                if (bank >= barrel.price) & (barrel.sku == small_red):
                     small_red_num += 1
                     bank -= barrel.price
-            elif (barrel.sku == small_green) & (cur_green_potions < 10):
-                if (bank >= barrel.price) & (barrel.quantity > 0):
+                elif (bank >= barrel.price) & (barrel.sku == mini_red):
+                    mini_red_num += 1
+                    bank -= barrel.price
+            elif (barrel.potion_type == [0, 100, 0, 0]) & (cur_green_potions < 10):
+                if (bank >= barrel.price) & (barrel.sku == small_green):
                     small_green_num += 1
                     bank -= barrel.price
-            elif (barrel.sku == small_blue) & (cur_blue_potions < 10):
-                if (bank >= barrel.price) & (barrel.quantity > 0):
+                elif (bank >= barrel.price) & (barrel.sku == mini_green):
+                    mini_green_num += 1
+                    bank -= barrel.price
+            elif (barrel.potion_type == [0, 0, 100, 0]) & (cur_blue_potions < 10):
+                if (bank >= barrel.price) & (barrel.sku == small_blue):
                     small_blue_num += 1
+                    bank -= barrel.price
+                elif (bank >= barrel.price) & (barrel.sku == mini_blue):
+                    mini_blue_num += 1
                     bank -= barrel.price
         
         plan = []
+        if (mini_red_num > 0):
+            plan.append({
+            "sku": mini_red,
+            "quantity": mini_red_num,
+            })
         if (small_red_num > 0):
             plan.append({
             "sku": small_red,
             "quantity": small_red_num,
             })
+
+        if (mini_green_num > 0):
+            plan.append({
+            "sku": mini_green,
+            "quantity": mini_green_num,
+            })
         if (small_green_num > 0):
             plan.append({
             "sku": small_green,
             "quantity": small_green_num,
+            })
+
+        if (mini_blue_num > 0):
+            plan.append({
+            "sku": mini_blue,
+            "quantity": mini_blue_num,
             })
         if (small_blue_num > 0):
             plan.append({
